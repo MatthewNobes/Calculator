4b7269fb037060808733ed86f478627334e46b0e
MatthewNobes <mattnobes.personal@protonmail.com>
2017-10-08 19:13:54 +0100

    test
    test

---
+++ Calculator1/Calculator1/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Calculator1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbMul = new System.Windows.Forms.RadioButton();
+            this.rdbSub = new System.Windows.Forms.RadioButton();
+            this.rdbDiv = new System.Windows.Forms.RadioButton();
+            this.rdbPlus = new System.Windows.Forms.RadioButton();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(86, 25);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(197, 26);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Num1";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(86, 57);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(197, 26);
+            this.txtNum2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Num2";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(301, 25);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(74, 90);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbMul);
+            this.groupBox1.Controls.Add(this.rdbSub);
+            this.groupBox1.Controls.Add(this.rdbDiv);
+            this.groupBox1.Controls.Add(this.rdbPlus);
+            this.groupBox1.Location = new System.Drawing.Point(423, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 150);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operators";
+            // 
+            // rdbMul
+            // 
+            this.rdbMul.AutoSize = true;
+            this.rdbMul.Location = new System.Drawing.Point(22, 116);
+            this.rdbMul.Name = "rdbMul";
+            this.rdbMul.Size = new System.Drawing.Size(86, 24);
+            this.rdbMul.TabIndex = 3;
+            this.rdbMul.TabStop = true;
+            this.rdbMul.Text = "Multiply";
+            this.rdbMul.UseVisualStyleBackColor = true;
+            // 
+            // rdbSub
+            // 
+            this.rdbSub.AutoSize = true;
+            this.rdbSub.Location = new System.Drawing.Point(22, 86);
+            this.rdbSub.Name = "rdbSub";
+            this.rdbSub.Size = new System.Drawing.Size(95, 24);
+            this.rdbSub.TabIndex = 2;
+            this.rdbSub.TabStop = true;
+            this.rdbSub.Text = "Subtract";
+            this.rdbSub.UseVisualStyleBackColor = true;
+            // 
+            // rdbDiv
+            // 
+            this.rdbDiv.AutoSize = true;
+            this.rdbDiv.Location = new System.Drawing.Point(22, 56);
+            this.rdbDiv.Name = "rdbDiv";
+            this.rdbDiv.Size = new System.Drawing.Size(77, 24);
+            this.rdbDiv.TabIndex = 1;
+            this.rdbDiv.TabStop = true;
+            this.rdbDiv.Text = "Divide";
+            this.rdbDiv.UseVisualStyleBackColor = true;
+            // 
+            // rdbPlus
+            // 
+            this.rdbPlus.AutoSize = true;
+            this.rdbPlus.Location = new System.Drawing.Point(22, 28);
+            this.rdbPlus.Name = "rdbPlus";
+            this.rdbPlus.Size = new System.Drawing.Size(64, 24);
+            this.rdbPlus.TabIndex = 0;
+            this.rdbPlus.TabStop = true;
+            this.rdbPlus.Text = "Plus";
+            this.rdbPlus.UseVisualStyleBackColor = true;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(86, 89);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(197, 26);
+            this.txtAnswer.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Answer";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnGo);
+            this.groupBox2.Controls.Add(this.txtNum1);
+            this.groupBox2.Controls.Add(this.txtAnswer);
+            this.groupBox2.Controls.Add(this.txtNum2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(394, 134);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input";
+            // 
+            // Calculator1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(642, 176);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Calculator1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbMul;
+        private System.Windows.Forms.RadioButton rdbSub;
+        private System.Windows.Forms.RadioButton rdbDiv;
+        private System.Windows.Forms.RadioButton rdbPlus;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Calculator1/Calculator1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Calculator1 : Form
+    {
+        public Calculator1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (rdbPlus.Checked)
+            {
+                int Dig1 = (Convert.ToInt32(txtNum1.Text));
+                int Dig2 = (Convert.ToInt32(txtNum2.Text));
+                int Answer = Dig1 + Dig2;
+                txtAnswer.Text = (Convert.ToString(Answer));
+            }
+            if (rdbDiv.Checked)
+            {
+                int Dig1 = (Convert.ToInt32(txtNum1.Text));
+                int Dig2 = (Convert.ToInt32(txtNum2.Text));
+                int Answer = Dig1 / Dig2;
+                txtAnswer.Text = (Convert.ToString(Answer));
+            }
+            if (rdbMul.Checked)
+            {
+                int Dig1 = (Convert.ToInt32(txtNum1.Text));
+                int Dig2 = (Convert.ToInt32(txtNum2.Text));
+                int Answer = Dig1 * Dig2;
+                txtAnswer.Text = (Convert.ToString(Answer));
+            }
+            if (rdbSub.Checked)
+            {
+                int Dig1 = (Convert.ToInt32(txtNum1.Text));
+                int Dig2 = (Convert.ToInt32(txtNum2.Text));
+                int Answer = Dig1 - Dig2;
+                txtAnswer.Text = (Convert.ToString(Answer));
+            }
+        }
+
+        private void txtPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
